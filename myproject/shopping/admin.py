3b514caff9219e17e512f4aed7aca26daf3564ec
Regis Santos <rg3915@yahoo.com.br>
2020-12-25 01:36:07 -0300

    Modela Shopping

--- myproject/shopping/admin.py
+++ myproject/shopping/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Compra, Produto, Carrinho
 
-# Register your models here.
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'data')
+    search_fields = ('cliente',)
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'preco')
+    search_fields = ('nome',)
+
+
+@admin.register(Carrinho)
+class CarrinhoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'compra', 'quantidade', 'preco')
+    search_fields = ('compra__cliente', 'produto__nome')
--- myproject/shopping/models.py
+++ myproject/shopping/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Compra(models.Model):
+    cliente = models.CharField(max_length=100)
+    data = models.DateField()
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'compra'
+        verbose_name_plural = 'compras'
+
+    def __str__(self):
+        return self.cliente
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    preco = models.DecimalField('preço', max_digits=6, decimal_places=2)
+
+    class Meta:
+        ordering = ('nome',)
+        verbose_name = 'produto'
+        verbose_name_plural = 'produtos'
+
+    def __str__(self):
+        return self.nome
+
+
+class Carrinho(models.Model):
+    compra = models.ForeignKey(
+        Compra,
+        related_name='compras',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    produto = models.ForeignKey(
+        Produto,
+        related_name='produtos',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    quantidade = models.PositiveIntegerField()
+    preco = models.DecimalField('preço', max_digits=6, decimal_places=2)
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'carrinho'
+        verbose_name_plural = 'carrinhos'
+
+    def __str__(self):
+        return f'{self.pk}-{self.compra.pk}-{self.produto}'
