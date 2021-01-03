b7e64428e13a87ec2b9621883b198b60772bf897
Regis Santos <rg3915@yahoo.com.br>
2021-01-03 01:31:30 -0300

    Cypress input-form2

--- cypress/integration/input-form2.spec.js
+++ cypress/integration/input-form2.spec.js
@@ -5,16 +5,17 @@ describe('Input form', () => {
     cy.visit('http://localhost:8000/shopping/shopping/')
 
     for (var i = 0; i < 5; i++) {
+      cy.get('#btnAddLine').click()
       var randName = name[Math.floor(Math.random() * name.length)]
       let randQuantity = Math.floor(Math.random() * 20)
       let randPrice = Math.floor(Math.random() * 10)
-      cy.get('#productName').type(randName)
-      cy.get('#productQuantity').clear()
-      cy.get('#productQuantity').type(randQuantity)
-      cy.get('#productPrice').clear()
-      cy.get('#productPrice').type(randPrice)
+      cy.get('#name'+i).type(randName)
+      cy.get('#quantity'+i).clear()
+      cy.get('#quantity'+i).type(randQuantity)
+      cy.get('#price'+i).clear()
+      cy.get('#price'+i).type(randPrice)
       cy.wait(500)
-      cy.get('#btnSubmit').click()
+      // cy.get('#btnSubmit').click()
     }
   })
 })
