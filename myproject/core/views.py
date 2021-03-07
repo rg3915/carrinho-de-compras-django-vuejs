831b69552b12582769958a8d63140baf66936093
rg3915 <regis42santos@gmail.com>
2021-03-06 23:32:46 -0300

    v 0.5

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -7,33 +7,3 @@ from myproject.shopping.models import Product, Shop, Cart
 
 def index(request):
     return render(request, 'index.html')
-
-
-def api_product(request):
-    products = Product.objects.all()
-    data = [item.to_dict() for item in products]
-    response = {'data': data}
-    return JsonResponse(response)
-
-
-@csrf_exempt
-def api_shopping_items_add(request):
-    request = request.POST
-    customer = json.loads(request.get('customer'))
-    products = json.loads(request.get('products'))
-
-    shop = Shop.objects.create(customer=customer)
-
-    for product in products:
-        product_obj = Product.objects.get(pk=product['pk'])
-        quantity = product['quantity']
-        price = product['price']
-
-        Cart.objects.create(
-            shop=shop,
-            product=product_obj,
-            quantity=quantity,
-            price=price
-        )
-    response = {'data': shop.pk}
-    return JsonResponse(response)
